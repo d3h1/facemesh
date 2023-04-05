92de9352b0619daa46b310e3ba1ed72dece0bf7b
denicabaravdic <deni@phntmlabs.com>
2023-04-04 20:18:05 -0700

    pushing changes to git hub

--- src/App.js
+++ src/App.js
@@ -1,22 +1,99 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react'
 import './App.css';
+import "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh"; 
+import Webcam from "react-webcam";
+import {drawMesh} from "./utilities"
 
-function App() {
+function App()
+{
+
+  // Webcam and  webcam canvas is created and referenced here
+  const wcRef = useRef(null); 
+  const cRef = useRef(null); 
+
+  // Facemesh function created
+  const startPrgram = async () =>
+  {
+    const net = await facemesh.load
+    (
+      {
+      inputResolution: {width:1000, height:680}, scale:0.6
+      }
+    )
+    setInterval(() =>
+    {
+      detect(net)
+    }, 100) // This is being run every 100 miliseconds consistently updated the array
+  };
+
+  // Detection of facemesh begins here
+  const detect = async (net) =>
+  {
+    if (
+      typeof wcRef.updating !== "NOT DEFINED" // web cam reference if not defined will explain that it is UNDEFINED
+      && wcRef.updating !== null 
+      && wcRef.updating.video.readyState === 4
+    )
+      {
+
+        // Video Camera properties
+        const output = wcRef.updating.output;
+        const outputWidth = wcRef.updating.output.outputWidth;
+        const outputHeight = wcRef.updating.output.outputHeight;
+
+        
+        wcRef.updating.output.width = outputWidth; // Video Width
+        wcRef.updating.output.height = outputHeight; // Video Height
+
+        
+        cRef.uupdating.height = outputHeight; // Canvas width
+        cRef.updating.width = outputWidth; // Canvas height
+
+        // Facial Detections are made
+        const face = await net.estimateFaces(output);
+        console.log(face);
+
+        // Facial Detections are drawn
+        const ctx = cRef.updating.getContext('2d');
+        drawMesh(face, ctx);
+
+      }
+  };
+
+  startPrgram();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+        <Webcam 
+          ref={wcRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "centr",
+            zIndex: 9,
+            width: 1000,
+            height: 680,
+          }}
+      />
+      <canvas
+        ref={cRef}
+        style={{
+          position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "centr",
+            zIndex: 9,
+            width: 1000,
+            height: 680,
+        }}
+        />
       </header>
     </div>
   );
